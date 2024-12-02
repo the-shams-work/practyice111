b1a7bc9ecba77fa61a15a1f0a28e8597f5634f6b
Shams Tabrej Alam <shams.22scse1010782@galgotiasuniversity.edu.in>
2024-12-02 12:18:01 +0530

    Initial Commit

---
+++ practyice111Tests/practyice111Tests.swift
@@ -0,0 +1,17 @@
+//
+//  practyice111Tests.swift
+//  practyice111Tests
+//
+//  Created by Shams Tabrej Alam on 02/12/24.
+//
+
+import Testing
+@testable import practyice111
+
+struct practyice111Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
